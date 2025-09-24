542fdab2dff4eb5b3fbb12e31e7ece98a13398cb
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-09-24 13:23:59 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainAdminDashboardContainer from './components/MainAdminDashboardContainer.tsx';
-import { Loader } from 'lucide-react';
 import { Toaster } from 'react-hot-toast';
 <link href="/src/style.css" rel="stylesheet"></link>
 function App() {
 
   return (
     <>
-      <Loader />
       <BrowserRouter>
         <Toaster position="top-center" reverseOrder={false} />
         <Routes>
